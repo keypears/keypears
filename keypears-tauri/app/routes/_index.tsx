760d9c0c12c5ad5d1d24d5ecbd7e989e5f1f286d
Ryan X. Charles <ryan@ryanxcharles.com>
2025-08-31 17:15:13 -0500

    demo app seems to work with react router

--- keypears-tauri/app/routes/_index.tsx
+++ keypears-tauri/app/routes/_index.tsx
@@ -1,10 +1,42 @@
 import { MetaFunction, redirect } from "react-router";
 import type { Route } from "./+types/_index.js";
 import { $path } from "safe-routes";
+import { invoke } from "@tauri-apps/api/core";
+import { useState } from "react";
 
 // export async function clientLoader({ params }: Route.ClientLoaderArgs) {
 //   return redirect($path("/spa"));
 // }
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <div>
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <button type="submit">Greet</button>
+      </form>
+      <p>{greetMsg}</p>
+    </div>
+  );
+}
 
 export const meta: MetaFunction = () => {
   return [
@@ -15,5 +47,5 @@ export const meta: MetaFunction = () => {
 };
 
 export default function AppIndex() {
-  return <>Hello.</>;
+  return <App />;
 }
