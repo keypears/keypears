37930675a7cbf1952dc55c3d6176e376a00fd506
Ryan X. Charles <ryan@ryanxcharles.com>
2025-08-31 15:29:34 -0500

    better way to use theme

--- ts/www-keypears-com/app/root.tsx
+++ ts/www-keypears-com/app/root.tsx
@@ -32,7 +32,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body className="bg-ctp-base text-ctp-text">
+      <body>
         {children}
         <ScrollRestoration />
         <Scripts />
