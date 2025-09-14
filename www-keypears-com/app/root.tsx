bd03b5ca7a113f67ad55db8d5b6f2399f2178269
Ryan X. Charles <ryan@ryanxcharles.com>
2025-09-14 13:28:55 -0500

    even better handle light/dark

--- www-keypears-com/app/root.tsx
+++ www-keypears-com/app/root.tsx
@@ -42,13 +42,18 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <script
           dangerouslySetInnerHTML={{
             __html: `
-  (function() {
-    try {
-      var d = window.matchMedia("(prefers-color-scheme: dark)").matches;
-      var cl = document.documentElement.classList;
-      if (d) cl.add('dark'); else cl.remove('dark');
-    } catch(e){}
-  })();
+(function() {
+  try {
+    var mql = window.matchMedia('(prefers-color-scheme: dark)');
+    var root = document.documentElement;
+    function update() {
+      if (mql.matches) root.classList.add('dark');
+      else root.classList.remove('dark');
+    }
+    update();
+    mql.addEventListener('change', update);
+  } catch (e) {}
+})();
             `,
           }}
         />
