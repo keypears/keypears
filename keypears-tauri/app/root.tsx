a484de825e95b084e02e9ccc8a520a4e709a947f
Ryan X. Charles <ryan@ryanxcharles.com>
2025-09-14 17:48:38 -0500

    add dark/light script

--- keypears-tauri/app/root.tsx
+++ keypears-tauri/app/root.tsx
@@ -5,6 +5,29 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <script
+          dangerouslySetInnerHTML={
+            /* This script sets the initial theme based on the user's system
+             * preference. It runs before the React app hydrates to prevent a
+             * flash of incorrect theme. */
+            {
+              __html: `
+(function() {
+  try {
+    var mql = window.matchMedia('(prefers-color-scheme: dark)');
+    var root = document.documentElement;
+    function update() {
+      if (mql.matches) root.classList.add('dark');
+      else root.classList.remove('dark');
+    }
+    update();
+    mql.addEventListener('change', update);
+  } catch (e) {}
+})();
+            `,
+            }
+          }
+        />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
