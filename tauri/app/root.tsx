f597fb8adfdaccb52e92745037b541eb845184d8
Ryan X. Charles <ryan@ryanxcharles.com>
2025-10-04 12:04:50 -0500

    rename to app.css like the webapp

--- tauri/app/root.tsx
+++ tauri/app/root.tsx
@@ -6,7 +6,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "react-router";
-import "./style.css";
+import "./app.css";
 import type { Route } from "./+types/root";
 
 export function Layout({ children }: { children: React.ReactNode }) {
