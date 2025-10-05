9aad279032ff7cb681570736dfc5c2317364ad89
Ryan X. Charles <ryan@ryanxcharles.com>
2025-10-05 06:40:42 -0500

    fix hydration error

--- tauri/app/root.tsx
+++ tauri/app/root.tsx
@@ -17,7 +17,7 @@ export async function clientLoader() {
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <script
           dangerouslySetInnerHTML={
