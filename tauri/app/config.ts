db0c1339a48934a42e1e72f999fc4b42d3285968
Ryan X. Charles <ryan@ryanxcharles.com>
2025-10-25 16:16:34 -0500

    remove obsolete config.ts

--- tauri/app/config.ts
+++
@@ -1,4 +0,0 @@
-export const DOMAIN =
-  process.env.NODE_ENV === "production" ? "earthbucks.com" : "4189.localhost";
-export const NAME = "EarthBucks";
-export const DESCRIPTION = "Cryptocurrency for AI.";
