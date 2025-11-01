0d7e1beb3d5c4d3fbcb2779e9e3e9df9b2c79319
Ryan X. Charles <ryan@ryanxcharles.com>
2025-11-01 13:26:50 -0500

    db:push commands

---
+++ ts-node/drizzle-clear.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'drizzle-kit';
+
+export default {
+  schema: './src/db/clear-schema.ts',
+  out: './drizzle',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL || 'postgresql://keypears:keypears_dev@localhost:5432/keypears_main',
+  },
+} satisfies Config;
